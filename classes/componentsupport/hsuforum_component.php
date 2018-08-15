cd14aff837a140e339a7bee4d41c331f913cf368
Rafael Becerra <rafael.becerrarodriguez@blackboard.com>
2018-08-15 15:00:56 +0000

    INT-13455: Rename strings from Moodlerooms to Open and Blackboard Open LMS in Lang files, Add fixes for Behat tests
    INT-13455: Adding rebrand for Lang files inside Lang folder
    
    INT-13455: Rewrite copyright strings for Blackboard and Blackboard Open LMS

--- classes/componentsupport/hsuforum_component.php
+++ classes/componentsupport/hsuforum_component.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Html file replacement support for Moodlerooms forum module.
+ * Html file replacement support for Open Forum module.
  * @author    Guy Thomas <citricity@gmail.com>
  * @copyright Copyright (c) 2017 Blackboard Inc. (http://www.blackboard.com)
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -28,7 +28,7 @@ defined ('MOODLE_INTERNAL') || die();
 use tool_ally\local_file;
 
 /**
- * Html file replacement support for Moodlerooms forum module.
+ * Html file replacement support for Open Forum module.
  * @author    Guy Thomas <citricity@gmail.com>
  * @copyright Copyright (c) 2017 Blackboard Inc. (http://www.blackboard.com)
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
