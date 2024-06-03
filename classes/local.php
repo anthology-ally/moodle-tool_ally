e49ff185b73b717e262232a2fe9245a2951c233d
Matthias Opitz <opitz@gmx.de>
2024-06-03 14:43:30 +0100

    If no token string was found - as in PHPUnit tests - return an empty string. (#97)
    Co-authored-by: Matthias Opitz <m.opitz@ucl.ac.uk>

--- classes/local.php
+++ classes/local.php
@@ -222,7 +222,7 @@ class local {
      * @throws \dml_exception
      * @throws \webservice_access_exception
      */
-    public static function get_ws_token() {
+    public static function  get_ws_token() {
         $allyuser = self::get_ally_web_user();
         if (!$allyuser) {
             $msg = 'Ally web user (ally_webuser) does not exist. Has auto configure been run?';
@@ -239,6 +239,11 @@ class local {
             throw new \webservice_access_exception($msg);
         }
         $wstoken = reset($tokens);
+
+        // If no token was found - as in PHPUnit tests - return an empty string.
+        if (!isset($wstoken->token)) {
+            $wstoken->token = "";
+        }
         return $wstoken;
     }
 
